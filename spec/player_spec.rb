612f9bef4a505672ae56db1ad2435e8fdabe4f37
Randy Leighton <student@epicodus.com>
2014-08-22 15:47:35 -0700

    test for Player class creation numeric validation working

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -8,9 +8,9 @@ describe Player do
 
   it "should verify number is entered as a numeric input" do
     new_team = Team.new(name: 'manchester')
-    new_player = Player.new(name: 'george', number: 'ff', team: new_team)
+    new_player = Player.new(name: 'george', number: 'fff', team: new_team)
     new_player.save
-    expect(new_player.errors[:number]).to eq ['is not a number']
+    expect(new_player.errors[:number]).to eq ["is the wrong length (should be 2 characters)", "is not a number"]
 
   end
 
